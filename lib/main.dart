e89c9486cba29fb7ed26e3045bab91d7e2bb45f9
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-11-28 13:26:07 -0700

    Home page & Band Model
    - Se implemento la pagina de inicio y se creo el band model.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bandnamesapp/src/pages/home.dart';
 import 'package:flutter/material.dart';
  
 void main() => runApp(MyApp());
@@ -6,17 +7,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      initialRoute: 'home',
+      routes: <String, WidgetBuilder>{
+        'home': (BuildContext context) => Home(), 
+      },
     );
   }
 }
